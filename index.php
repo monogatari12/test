283244cb3489f714e634538075f7aa22fd5dcac0
monogatari12 <74235230+monogatari12@users.noreply.github.com>
2020-11-20 12:10:15 +0900

    文字化け修正チャレンジその3

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <title></title>
 </haed>
 <body>
-    <?php phpinfo(); ?>
+    /*<?php phpinfo(); ?>/*
     <?php include_once("index.html"); ?>
+ <?php include_once("class_room_1.html"); ?>
 </body>
 </html>
