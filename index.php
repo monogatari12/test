a3d499479704923e7e58fc5b0ab3af2d9cc05a66
monogatari12 <user@yuzanoMacBook-Air.local>
2020-11-22 11:46:08 +0900

    文字化け対処完了

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <title></title>
 </haed>
 <body>
-    /*<?php phpinfo(); ?>/*
+
     <?php include_once("index.html"); ?>
- <?php include_once("class_room_1.html"); ?>
+    <?php include_once("class_room_1.html"); ?>
 </body>
 </html>
